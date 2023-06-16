ec5502155abda9750ab7ff21ce5c4747f274fa6b
nicalol <131474586+nicalol@users.noreply.github.com>
2023-06-15 20:08:48 -0400

    Add files via upload

---
+++ java.js
@@ -0,0 +1,37 @@
+document.querySelectorAll('.image-container img').forEach(image => {
+
+    image.onclick = (event) => {
+        event.stopPropagation();
+
+        const text = image.getAttribute('data-text');
+        const popUp = document.querySelector('.popup-text');
+        popUp.style.display = 'block';
+        popUp.querySelector('p').innerText = text;
+    };
+
+});
+
+document.querySelector('.popup-text').onclick = (event) => {
+    event.stopPropagation();
+};
+
+document.addEventListener('click', (event) => {
+    const popUp = document.querySelector('.popup-text');
+    if (popUp.style.display === 'block' && !popUp.contains(event.target)) {
+        popUp.style.display = 'none';
+    }
+});
+
+function closePopup() {
+    const popUp = document.querySelector('.popup-text');
+    popUp.style.animationName = 'slideout';
+    popUp.style.animationDuration = '0.8s';
+    popUp.style.animationTimingFunction = 'ease-in';
+    popUp.style.animationFillMode = 'forwards';
+
+    setTimeout(() => {
+        popUp.style.display = 'none';
+        popUp.style.animationTimingFunction = 'ease-out';
+        popUp.style.animationName = '';
+    }, 800);
+}
